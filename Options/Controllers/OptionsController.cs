823a768d968ae746589ee8ad7ed6ac4ea7b99435
Izuchukwu Okorie <Izuchukwu.Okorie@wemabank.com>
2024-01-18 21:18:06 +0100

    OptionsPattern

---
+++ Options/Controllers/OptionsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Options.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OptionsController : ControllerBase
+    {
+        private readonly PlayerSettings _playersettings;
+
+        public OptionsController(IOptions<PlayerSettings> playersettings)
+        {
+            _playersettings = playersettings.Value;
+        }
+        [HttpGet("Player")]
+        public ActionResult GetPlayerSettings()
+        {
+            return Ok(_playersettings);
+        }
+    }
+}
---
+++ Options/PlayerSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Options
+{
+    public class PlayerSettings
+    {
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+    }
+}
